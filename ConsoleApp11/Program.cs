a09674580a557d5a509b68486512a162b6a03b5d
LolKek46578 <maks.borodynkin@list.ru>
2023-11-19 19:45:54 +0300

    yhjtjgh

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -1,4 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("Hello, Wfdfdfd!");
+Console.WriteLine("Hello, Wfdfdfd!");
+Console.WriteLine("Hello, Wfdfdfd!");
+Console.WriteLine("Hello, Wfdfdfd!");
+Console.WriteLine("Hello, Wfdfdfd!");
+Console.WriteLine("Hello, Wfdfdfd!");
+Console.WriteLine("Hello, Wfdfdfd!");
+Console.WriteLine("Hello, Wfdfdfd!");
+Console.WriteLine("Hello, Wfdfdfd!");
+Console.WriteLine("Hello, Wfdfdfd!");
+Console.WriteLine("Hello, Wfdfdfd!");
+Console.WriteLine("Hello, Wfdfdfd!");
+Console.WriteLine("Hello, Wfdfdfd!");
+
+Console.WriteLine("Hello, Wfdfdfd!");
+
+Console.WriteLine("Hello, Wfdfdfd!"); 
+
+Console.WriteLine("Hello, Wfdfdfd!");
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
